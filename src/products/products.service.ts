651bcb8730b75085d6c106ac49807e077e75a88a
carlosismaelad <carlosismaelad@outlook.com>
2024-03-18 00:11:35 -0300

    Initial commit - class, controller, service and DTO

---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/Product.entity';
+
+@Injectable()
+export class ProductsService {
+    private products: Product[] = [
+        {
+            id: 1,
+            name: "Pulseira praiana",
+            description: "Pulseira feita com conchas e pérolas.",
+            price: 6.00,
+            tags: ["praia", "dia", "sol"]
+        }
+    ]
+
+    findAll(){
+        return this.products
+    }
+
+    findOne(id: string){
+        const existingProduct = this.products.find((product: Product) => product.id === +id)
+
+        if(!existingProduct){
+            throw new HttpException(`Produto id ${id} não encontrado.`, HttpStatus.NOT_FOUND)
+        }
+
+        return existingProduct
+    }
+
+    create(createProductDto: any){
+        this.products.push(createProductDto)
+    }
+
+    update(id: string, updateProductDto: any){
+        const indexProduct = this.products.findIndex(product => product.id === +id)
+        this.products[indexProduct] = updateProductDto
+    }
+
+    remove(id: string){
+        const indexProduct = this.products.findIndex(product => product.id === +id)
+        
+        if(indexProduct >= 0){
+            this.products.splice(indexProduct, 1)
+        }
+    }
+
+
+}
